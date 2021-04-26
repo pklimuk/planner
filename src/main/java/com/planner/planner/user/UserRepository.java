e200ba562bf5f8b0aa291fe64077ac8f8617cf0e
Pavel Klimuk <pavelklimuk@yahoo.com>
2021-04-26 13:20:19 +0200

    Changed UserRepository

--- src/main/java/com/planner/planner/user/UserRepository.java
+++ src/main/java/com/planner/planner/user/UserRepository.java
@@ -1,7 +1,14 @@
 package com.planner.planner.user;
 
 import com.planner.planner.user.User;
+import com.planner.planner.userProfile.UserProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT up FROM Users up WHERE up.login = ?1")
+    Optional<User> findUserByLogin(String login);
 }
