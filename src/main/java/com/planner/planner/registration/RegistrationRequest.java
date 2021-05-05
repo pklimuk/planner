805b3c21c1966883d2dada1686f9aeba82d83af3
Pavel Klimuk <pavelklimuk@yahoo.com>
2021-05-05 21:12:26 +0200

    Added Registration package

---
+++ src/main/java/com/planner/planner/registration/RegistrationRequest.java
@@ -0,0 +1,21 @@
+package com.planner.planner.registration;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Getter
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class RegistrationRequest {
+    private final String firstName;
+    private final String lastName;
+    private final String email;
+    private final LocalDate dob;
+    private final String login;
+    private final String password;
+}
