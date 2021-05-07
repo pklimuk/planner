9d89ee9cb6e19d1cf9729410175611c5569e7e19
Pavel Klimuk <pavelklimuk@yahoo.com>
2021-05-07 12:48:48 +0200

    Changed "extends" parameter to JpaRepository

--- src/main/java/com/planner/planner/deadline/DeadlineRepository.java
+++ src/main/java/com/planner/planner/deadline/DeadlineRepository.java
@@ -1,7 +1,8 @@
 package com.planner.planner.deadline;
 
 import com.planner.planner.deadline.Deadline;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface DeadlineRepository extends CrudRepository<Deadline, Long> {
+public interface DeadlineRepository extends JpaRepository<Deadline, Long> {
 }
